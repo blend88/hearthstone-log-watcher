30d64793792bc2611695eb477955dfc97577682e
Manas Paldhe <mpaldhe@clipmineinc.com>
2016-01-21 23:17:32 -0800

    Testing on alpha - working fine

--- tracker.js
+++ tracker.js
@@ -1,34 +1,87 @@
+var request = require('request');
+
 var LogWatcher = require('hearthstone-log-watcher');
 var logWatcher = new LogWatcher();
 
+var buffer = '';
+var twitch_user_name = 'test_twitch_username';
+var start_time;
+var end_time;
+
+var reset_buffer = function() {
+  buffer = '';
+}
+
+var add_to_buffer = function(line) {
+  buffer += '\n'+line;
+}
+
+var send_buffer = function() {
+  end_time = Math.floor(Date.now() / 1000)
+  console.log(buffer);
+  var url = 'https://alpha.clip.mn/1/hs_client_metadata/'
+
+  var data = {
+    twitch_user_name: twitch_user_name,
+    start_time:  start_time,
+    end_time: end_time,
+    data: buffer
+  }
+  request.post({
+    url: url,
+    json: true,
+    headers: {'content-type' : 'application/json'},
+    body: data },
+    function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        console.log("Success");
+        console.log(body);
+      } else{
+        console.log("Fail");
+        console.log(body);
+      }
+    }
+  );
+}
+
 logWatcher.on('zone-change', function (data) {
-  console.log(Math.floor(Date.now() / 1000) + ': ' + data.cardName + ' has moved from ' + data.fromTeam + ' ' + data.fromZone + ' to ' + data.toTeam + ' ' + data.toZone);
+  var line = Math.floor(Date.now() / 1000) + ': zone-change - ' + data.cardName + ' has moved from ' + data.fromTeam + ' ' + data.fromZone + ' to ' + data.toTeam + ' ' + data.toZone;
+  add_to_buffer(line);
 });
 
 logWatcher.on('game-start', function (data) {
+  start_time = Math.floor(Date.now() / 1000)
   var friendly_player = data[0],
       enemy_player = data[1];
 
-  console.log(Math.floor(Date.now() / 1000) + ": starting a game - " + friendly_player.name + " v/s " + enemy_player.name);
-  console.log(Math.floor(Date.now() / 1000) + " " + friendly_player.name + " has id: "  + friendly_player.id);
-  console.log(Math.floor(Date.now() / 1000) + " " + enemy_player.name + " has id: "  + enemy_player.id);
+  var line = Math.floor(Date.now() / 1000) + ": starting a game - " + friendly_player.name + " v/s " + enemy_player.name;
+  add_to_buffer(line);
+  var line = Math.floor(Date.now() / 1000) + ": starting a game - " + friendly_player.name + " has id = "  + friendly_player.id;
+  add_to_buffer(line);
+  var line = Math.floor(Date.now() / 1000) + ": starting a game - " + enemy_player.name + " has id = "  + enemy_player.id;
+  add_to_buffer(line);  
 });
 
 logWatcher.on('game-over', function (data) {
   var friendly_player = data[0],
       enemy_player = data[1];
-  console.log("game ended - " + friendly_player.name + " result = " + friendly_player.status);
-  console.log("game ended - " + enemy_player.name + " result = " + enemy_player.status);
+  var line = Math.floor(Date.now() / 1000) + ": game ended - " + friendly_player.name + " result = " + friendly_player.status;
+  add_to_buffer(line);
+  var line = Math.floor(Date.now() / 1000) + ": game ended - " + enemy_player.name + " result = " + enemy_player.status;
+  add_to_buffer(line);
+  send_buffer();  
 });
 
 
 logWatcher.on('hero-update', function (data) {
-  console.log(Math.floor(Date.now() / 1000) + ': hero-update: ' + data.playerId + ' is hero: ' + data.heroName + ' is friendly: ' + data.friendly);
+  var line = Math.floor(Date.now() / 1000) + ': hero-update: ' + data.playerId + ' is hero = ' + data.heroName + ' is friendly = ' + data.friendly;
+  add_to_buffer(line);
 });
 
 
 
 
 console.log('starting tracker.js')
+
 logWatcher.start();
 
