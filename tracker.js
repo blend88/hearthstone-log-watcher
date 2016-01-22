908ae9602f1f946e792ef51ee371980f7b61558e
Manas Paldhe <mpaldhe@clipmineinc.com>
2016-01-22 13:30:06 -0800

    Adding _ to events, sending version on game start

--- tracker.js
+++ tracker.js
@@ -46,7 +46,7 @@ var send_buffer = function() {
 }
 
 logWatcher.on('zone-change', function (data) {
-  var line = Math.floor(Date.now()) + ': zone change - ' + data.cardName + ' has moved from ' + data.fromTeam + ' ' + data.fromZone + ' to ' + data.toTeam + ' ' + data.toZone;
+  var line = Math.floor(Date.now()) + ': zone_change : ' + data.cardName + ' has moved from ' + data.fromTeam + ' ' + data.fromZone + ' to ' + data.toTeam + ' ' + data.toZone;
   add_to_buffer(line);
 });
 
@@ -55,39 +55,39 @@ logWatcher.on('game-start', function (data) {
   var friendly_player = data[0],
       enemy_player = data[1];
 
-  var line = Math.floor(Date.now()) + ": starting a game - " + friendly_player.name + " v/s " + enemy_player.name;
+  add_to_buffer(Math.floor(Date.now()) +  ': game_start : version = ' + version);
+  var line = Math.floor(Date.now()) + ": game_start : " + friendly_player.name + " v/s " + enemy_player.name;
   add_to_buffer(line);
-  var line = Math.floor(Date.now()) + ": starting a game - " + friendly_player.name + " has id = "  + friendly_player.id;
+  var line = Math.floor(Date.now()) + ": game_start : " + friendly_player.name + " has id = "  + friendly_player.id;
   add_to_buffer(line);
-  var line = Math.floor(Date.now()) + ": starting a game - " + enemy_player.name + " has id = "  + enemy_player.id;
+  var line = Math.floor(Date.now()) + ": game_start : " + enemy_player.name + " has id = "  + enemy_player.id;
   add_to_buffer(line);  
 });
 
 logWatcher.on('game-over', function (data) {
   var friendly_player = data[0],
       enemy_player = data[1];
-  var line = Math.floor(Date.now()) + ": game ended - " + friendly_player.name + " result = " + friendly_player.status;
+  var line = Math.floor(Date.now()) + ": game ended : " + friendly_player.name + " result = " + friendly_player.status;
   add_to_buffer(line);
-  var line = Math.floor(Date.now()) + ": game ended - " + enemy_player.name + " result = " + enemy_player.status;
+  var line = Math.floor(Date.now()) + ": game ended : " + enemy_player.name + " result = " + enemy_player.status;
   add_to_buffer(line);
   send_buffer();  
 });
 
 
 logWatcher.on('hero-update', function (data) {
-  var line = Math.floor(Date.now()) + ': hero-update - player_id = ' + data.playerId + ' has hero = ' + data.heroName ;
+  var line = Math.floor(Date.now()) + ': hero_update : player_id = ' + data.playerId + ' has hero = ' + data.heroName ;
   add_to_buffer(line);
   var friendly = '';
   if (data.friendly) {
     friendly = 'not';
   }
-  var line =  Math.floor(Date.now()) + ': hero-update - player_id = ' + data.playerId + ' is ' + friendly + ' friendly';
+  var line =  Math.floor(Date.now()) + ': hero_update : player_id = ' + data.playerId + ' is ' + friendly + ' friendly';
   add_to_buffer(line);
 });
 
 
 
 
-add_to_buffer('starting tracker.js: version = ' + version);
 logWatcher.start();
 
